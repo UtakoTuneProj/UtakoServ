0ee1ca8b9b2fd5eeb14f1362ea5cb51f8321d530
Nanamachi <7machi@nanamachi.net>
2016-11-12 12:18:03 +0900

    Tweepy完成: 結果を投げればよき感じでツイートしてくれるように

--- tweepyCore.py
+++ tweepyCore.py
@@ -12,3 +12,7 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 
 api = tweepy.API(auth)
 tweet = lambda x:api.update_status(status = x)
+
+def chart_tw(stage,est,mvid,title):
+    content = '[予想スコア:{0:.1f}pts / {1:.0f}時間経過]\n{2}\nhttp://nico.ms/{3} #{3}'.format(est, stage, title, mvid)
+    tweet(content)
