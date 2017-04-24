87c94c8ddfeea1937cb933c2a27391378257b441
Nanamachi <7machi@nanamachi.net>
2017-04-25 02:30:40 +0900

    Add SQLexchanger: file to exchange data from json to SQL

---
+++ SQLexchanger.py
@@ -0,0 +1,15 @@
+import UtakoChartInitializer
+import UtakoServCore as core
+from progressbar import ProgressBar
+
+if __name__ == '__main__':
+    cf = core.Chartfile()
+    dat = cf.read()
+    p = ProgressBar(0, len(dat))
+
+    db = core.DataBase()
+
+    for i, mvid in enumerate(dat):
+        p.update(i+1)
+        for j, cell in enumerate(dat[mvid]):
+            db.setChart(mvid, j, *cell)
