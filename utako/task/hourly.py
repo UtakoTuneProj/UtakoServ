d36c3ccfcd20018c23ea99c905736babffd13f46
Nanamachi <7machi@nanamachi.net>
2017-12-24 15:30:29 +0900

    add task/: separate task dir.

--- utako/task/hourly.py
+++ utako/task/hourly.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# ServCore: core module for hourly task
+from utako.common_import import *
 
 from utako.presenter.chart_updater import ChartUpdater
 from utako.presenter.status_updater import StatusUpdater
 
-def main():
+def hourly():
     StatusUpdater()()
     ChartUpdater()()
 
-    return 0
-
-# sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='UTF-8')
-if __name__ == '__main__':
-    main()
+    return None
