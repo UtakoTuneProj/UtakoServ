d557843d17fc6ca4d6f1c7fe6aebe762edc5b8cf
Nanamachi <town7.haruki@gmail.com>
2018-04-13 14:40:05 +0900

    change conf/auth.conf.example: sqlite, postgres and mysql are available

--- utako/model/abstract_model.py
+++ utako/model/abstract_model.py
@@ -3,7 +3,14 @@
 from peewee import *
 from utako.common_import import config
 
-database = MySQLDatabase(**config['database'])
+if config['database_type']['dbtype'] == 'mysql':
+    database_class = MySQLDatabase
+elif config['database_type']['dbtype'] == 'postgres':
+    database_class = PostgresqlDatabase
+elif config['database_type']['dbtype'] == 'sqlite':
+    database_class = SqliteDatabase
+
+database = database_class(**config['database'])
 
 class UnknownField(object):
     def __init__(self, *_, **__): pass
