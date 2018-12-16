5f84a13c290a97b82d63fa3757199e442a81cb0a
Nanamachi <7machi@nanamachi.net>
2018-12-17 04:02:42 +0900

    change song_score_updater: add forward prediction

--- utako/presenter/song_score_updater.py
+++ utako/presenter/song_score_updater.py
@@ -10,9 +10,13 @@ from utako.presenter.xml_reader import XmlReader
 from utako.exception.mov_deleted_exception import MovDeletedException
 from utako.exception.no_response_exception import NoResponseException
 
-VIEW_WEIGHT = np.array((0.68, 0.16, 0.017, -0.15, 0.69))
-COMMENT_WEIGHT = np.array((-0.066, 0.86, 0.064, -0.041, 0.19))
-MYLIST_WEIGHT = np.array((-0.04, 0.14, 0.8, -0.03, 0.14))
+BACKWARD_VIEW_WEIGHT = np.array((0.68, 0.16, 0.017, -0.15, 0.69))
+BACKWARD_COMMENT_WEIGHT = np.array((-0.066, 0.86, 0.064, -0.041, 0.19))
+BACKWARD_MYLIST_WEIGHT = np.array((-0.04, 0.14, 0.8, -0.03, 0.14))
+
+FORWARD_VIEW_WEIGHT = np.array((0.90, 0.028, 0.19, -0.41, 1.62))
+FORWARD_COMMENT_WEIGHT = np.array((0.20, 0.83, 0.068, -0.28, 0.70))
+FORWARD_MYLIST_WEIGHT = np.array((0.18, 0.022, 0.95, -0.26, 0.64))
 
 class SongScoreUpdater:
     def __call__(self, mvid): #ランキング取得・キュー生成部
@@ -34,21 +38,32 @@ class SongScoreUpdater:
                 return
                 
             days = (datetime.datetime.now() - ret["first_retrieve"]).days
-            predict_args = np.array((
-                ret['view_counter'],
-                ret['comment_num'],
-                ret['mylist_counter'],
-                days,
-                10
-            ))
             if days >= 7:
-                view = ( predict_args ** VIEW_WEIGHT ).prod()
-                comment = ( predict_args ** COMMENT_WEIGHT ).prod()
-                mylist = ( predict_args ** MYLIST_WEIGHT ).prod()
+                predict_args = np.array((
+                    ret['view_counter'] + 1,
+                    ret['comment_num'] + 1,
+                    ret['mylist_counter'] + 1,
+                    days,
+                    10
+                ))
+                view = ( predict_args ** BACKWARD_VIEW_WEIGHT ).prod()
+                comment = ( predict_args ** BACKWARD_COMMENT_WEIGHT ).prod()
+                mylist = ( predict_args ** BACKWARD_MYLIST_WEIGHT ).prod()
                 score_status = -1
 
             else:
-                return
+                minutes = (datetime.datetime.now() - ret["first_retrieve"]).total_seconds() / 60
+                predict_args = np.array((
+                    ret['view_counter'] + 1,
+                    ret['comment_num'] + 1,
+                    ret['mylist_counter'] + 1,
+                    minutes + 1,
+                    10
+                ))
+                view = ( predict_args ** FORWARD_VIEW_WEIGHT ).prod()
+                comment = ( predict_args ** FORWARD_COMMENT_WEIGHT ).prod()
+                mylist = ( predict_args ** FORWARD_MYLIST_WEIGHT ).prod()
+                score_status = 1
 
         else:
             view = chart.view
