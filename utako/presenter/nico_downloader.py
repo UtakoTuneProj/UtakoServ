19aa565e6c63576976a1ec831bf60aa55d877547
Nanamachi <7machi@nanamachi.net>
2019-01-26 10:39:37 +0900

    change nico_downloader: selectable save dir

--- utako/presenter/nico_downloader.py
+++ utako/presenter/nico_downloader.py
@@ -5,10 +5,10 @@ import subprocess as sbproc
 import youtube_dl
 
 class NicoDownloader:
-    def __call__(self, mvid, retries = 5, force = False): #ランキング取得・キュー生成部
+    def __call__(self, mvid, retries = 5, force = False, writepath = 'tmp/'): #ランキング取得・キュー生成部
         try:
             youtube_dl.YoutubeDL({
-                'outtmpl': 'tmp/mp4/%(id)s.mp4',
+                'outtmpl': writepath + 'mp4/%(id)s.mp4',
                 'retries': retries,
             }).download([
                 'http://www.nicovideo.jp/watch/{}'.format(mvid)
@@ -19,12 +19,12 @@ class NicoDownloader:
                 raise
             else:
                 time.sleep(10)
-                self(mvid, retries = retries - 1, force = force)
+                self(mvid, retries = retries - 1, force = force, writepath=writepath)
         else:
             process = sbproc.run([
                 'ffmpeg',
                 '-i', #infile
-                'tmp/mp4/{}.mp4'.format(mvid),
+                writepath + 'mp4/{}.mp4'.format(mvid),
                 '-y' if force else '-n', #overwrite if force is True
                 '-aq', #bitrate
                 '128k',
@@ -32,9 +32,5 @@ class NicoDownloader:
                 '1', #monoral
                 '-ar', #sampling rate
                 '44100',
-                'tmp/wav/{}.wav'.format(mvid),#outfile name
+                writepath + 'wav/{}.wav'.format(mvid),#outfile name
             ])
-            if process.returncode != 0: # if process does not stop accurately
-                os.remove('tmp/mp4/{}.mp4'.format(mvid))
-                time.sleep(10)
-                self(mvid, retries = retries, force = force)
