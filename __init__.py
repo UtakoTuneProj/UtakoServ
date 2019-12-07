16e1a8096ae5957a6bda5ba13066413afc93089d
Nanamachi <7machi@nanamachi.net>
2019-12-07 21:48:16 +0900

    fix __init__.py: adopt new dataset file architecture

--- __init__.py
+++ __init__.py
@@ -7,10 +7,25 @@ import numpy as np
 import gc
 import chainer
 import importlib as imp
+from pathlib import Path
 
-wav = np.load('datasets/wav.major.npy', mmap_mode='r')
+DATASET_DIR="datasets"
+DATASET_NAME="chronicle.2019.03"
+
+basedir=Path(DATASET_DIR) / DATASET_NAME
+dataset_info = json.load((basedir/'info.json').open())
+
+wav = np.memmap(
+    str( basedir/'wav.memmap' ),
+    mode='r',
+    dtype=np.float32
+).reshape((-1,1,dataset_info['length_per_sample']))
+songs_label = np.load(
+    str(basedir/'label.npy'),
+    mmap_mode='r'
+)
+
 tags_label = np.load('datasets/wav.tags.npy', mmap_mode='r')
-songs_label = np.load('datasets/wav.major.npy.label.npy', mmap_mode='r')
 with open('datasets/wav.tags.npy.order.super.json') as f:
     sp_tags = json.load(f)
 
