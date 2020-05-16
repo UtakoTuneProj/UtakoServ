2b229c89faf1b7f570f2eb912d7523b38bcac514
Nanamachi <7machi@nanamachi.net>
2020-05-16 21:55:56 +0900

    feat(utako.root_logger): introduce logging

--- utako/__init__.py
+++ utako/__init__.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import logging
+
+class UtakoLogFormatter(logging.Formatter):
+    def __init__(self,
+        fmt='[UTAKO | %(levelname)s | %(module)s.%(funcName)s | %(asctime)-15s] %(message)s',
+        datefmt=None,
+        style='%'
+    ):
+        super().__init__(fmt, datefmt, style)
+
+def get_root_logger():
+    logger = logging.getLogger('utako')
+
+    logger.propagate = True
+    logger.setLevel('DEBUG')
+    logger.addHandler(logging.StreamHandler())
+    logger.handlers[-1].setFormatter(UtakoLogFormatter())
+    logger.debug('New Logger has been set')
+
+    return logger
+
+root_logger = get_root_logger()
 
 import utako.common_import
 import utako.model
