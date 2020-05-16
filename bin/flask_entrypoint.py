ddc5d21cccf4090a4400a4a3fddf3fbc67ad6499
Nanamachi <7machi@nanamachi.net>
2020-05-16 21:54:28 +0900

    fix(flask_entrypoint): define response body

--- bin/flask_entrypoint.py
+++ bin/flask_entrypoint.py
@@ -16,16 +16,22 @@ def index():
 def analyze(movie_id):
     count = request.args.get('count', 1, type=int)
     utako.presenter.song_index_updater.SongIndexUpdater()(limit = count)
-    return
+    return {
+        'status': 'complete'
+    }
 
 @app.route('/trigger/hourly', methods=['POST'])
 def hourly():
     utako.task.hourly.hourly()
-    return
-
+    return {
+        'status': 'complete'
+    }
 
 @app.route('/trigger/recreate_song_relations', methods=['POST'])
 def recreate_song_relations():
     task = utako.presenter.song_relation_constructor.SongRelationConstructor()
     task(max_relations=13)
-    return
+    return {
+        'status': 'complete'
+    }
+
