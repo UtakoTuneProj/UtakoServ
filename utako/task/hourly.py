7159aae4923adf8f0854d3c5d2068694e4185fff
Nanamachi <7machi@nanamachi.net>
2020-05-17 23:11:07 +0900

    feat(task.hourly): introduce logger

--- utako/task/hourly.py
+++ utako/task/hourly.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from utako.common_import import *
+from utako import root_logger as logger
 
 from utako.presenter.status_updater import StatusUpdater
 from utako.presenter.chart_updater import ChartUpdater
@@ -10,8 +11,23 @@ from utako.model.abstract_model import database
 def hourly():
     try:
         status_results = StatusUpdater()()
+        logger.info(
+            'StatusUpdater finished. Inserted Rows: %i',
+            status_results['inserted_row_counts']
+        )
+
         chart_results = ChartUpdater()()
+        logger.info(
+            'ChartUpdater finished. Today: %i, LastWeek: %i',
+            len(chart_results['today']),
+            len(chart_results['lastweek'])
+        )
+
         score_results = ScoreUpdater()()
+        logger.info(
+            'ScoreUpdater finished. Updated Score: %i',
+            len(score_results)
+        )
     finally:
         database.close()
 
