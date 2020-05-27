8ee8a38a1197b876b5bf30f556a10a49259b4c15
Nanamachi <7machi@nanamachi.net>
2020-05-28 00:13:21 +0900

    test(ChartUpdater): add record assertion

--- tests/presenter/chart_updater.py
+++ tests/presenter/chart_updater.py
@@ -1,42 +1,87 @@
-import datetime
+from datetime import datetime
+from datetime import timedelta
+import pytest
+import freezegun
 
 from utako.presenter.chart_updater import ChartUpdater
 
+from utako.model.chart import Chart
+from utako.model.status import Status
+
+NOW = datetime.now()
+
 class CaptiveChartUpdater:
-    def should_return_without_exception(
+    @freezegun.freeze_time(NOW)
+    @pytest.mark.parametrize(
+        'status_data,expect_to_insert_chart,expected_validity',[
+        ([  # first retrieved
+            'sm98765432', 0, 0, 1, None,
+            NOW - timedelta(hours=0)
+        ],  True, True),
+
+        ([  # first retrieved (with delay margin)
+            'sm98765432', 0, 0, 1, None,
+            NOW - timedelta(hours=1, minutes=30)
+        ],  True, True),
+
+        ([  # first retrieved (over delay margin)
+            'sm98765432', 0, 0, 1, None,
+            NOW - timedelta(hours=1, minutes=30, seconds=1)
+        ],  True, False),
+
+        ([  # too early update
+            'sm98765432', 23, 0, 1, None,
+            NOW - timedelta(hours=23, seconds=-1)
+        ],  True, False),
+
+        ([  # last epoch at postday
+            'sm98765432', 23, 0, 1, None,
+            NOW - timedelta(hours=23)
+        ],  True, True),
+
+        ([  # after a day
+            'sm98765432', 24, 0, 1, None,
+            NOW - timedelta(hours=24)
+        ],  False, True),
+
+        ([  # before a week
+            'sm98765432', 24, 0, 1, None,
+            NOW - timedelta(days=7, hours=1, seconds=-1)
+        ],  False, True),
+
+        ([  # update on after a week
+            'sm98765432', 24, 0, 1, None,
+            NOW - timedelta(days=7, hours=1)
+        ],  True, True),
+
+        ([  # update on after a week (with delay margin)
+            'sm98765432', 24, 0, 1, None,
+            NOW - timedelta(days=7, hours=2, minutes=30)
+        ],  True, True),
+
+        ([  # too late for update on after a week
+            'sm98765432', 24, 0, 1, None,
+            NOW - timedelta(days=7, hours=2, minutes=30, seconds=1)
+        ],  True, False),
+        ])
+    def should_insert_chart_record_from_thumb(
         self,
         inject_mock_getthumbinfo,
         inject_status,
+        status_data,
+        expect_to_insert_chart,
+        expected_validity,
     ):
-        inject_status([[
-            'sm1', 0, 0, 1, None,
-            datetime.datetime.now() - datetime.timedelta(minutes = 30)
-            # first retrieved
-        ],[
-            'sm2', 1, 0, 1, None,
-            datetime.datetime.now() - datetime.timedelta(minutes = 90)
-            # second epoch
-        ],[
-            'sm3', 23, 0, 1, None,
-            datetime.datetime.now() - datetime.timedelta(hours=23, minutes = 30)
-            # on the post day last epoch
-        ],[
-            'sm4', 24, 0, 1, None,
-            datetime.datetime.now() - datetime.timedelta(days=1, minutes = 30)
-            # after a day, do not track until a week passed
-        ],[
-            'sm5', 24, 0, 1, None,
-            datetime.datetime.now() - datetime.timedelta(days=6, hours=23, minutes = 30)
-            # after a day, do not track until a week passed
-        ],[
-            'sm6', 24, 0, 1, None,
-            datetime.datetime.now() - datetime.timedelta(days=7, minutes = 30)
-            # last track, passed a week
-        ],[
-            'sm7', 25, 1, 1, None,
-            datetime.datetime.now() - datetime.timedelta(days=7, hours=1, minutes = 30)
-            # after a week, the movie will not be tracked
-        ]])
+        inject_status([status_data])
 
         ChartUpdater()()
+        assert Chart.select().exists() == expect_to_insert_chart
 
+        status_record = Status.select().first()
+        expected_epoch = status_data[1] + int(expect_to_insert_chart)
+        expected_iscomplete = (( expected_epoch == 25 ) and expected_validity )
+
+        assert status_record.validity == expected_validity
+        assert status_record.epoch == expected_epoch
+        assert status_record.iscomplete == expected_iscomplete
+        assert ( status_record.analyzegroup is None ) ^ expected_iscomplete
