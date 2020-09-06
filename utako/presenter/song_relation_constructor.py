5ea748d75d6b01bb46a40a5e1cd1cc91669c41a4
Nanamachi <7machi@nanamachi.net>
2020-09-06 20:43:14 +0900

    feat(SongRelCon): can create tree record for specified songs

--- utako/presenter/song_relation_constructor.py
+++ utako/presenter/song_relation_constructor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from utako import root_logger
 from utako.common_import import *
 from utako.model.abstract_model import database
 from utako.model.song_index import SongIndex
@@ -13,6 +14,11 @@ class SongRelationConstructor:
         relations = self.get_relations(positions, max_relations)
         self.store_all_relations(movies, relations)
 
+    def recalculate_for(self, movie_id, max_relations=11):
+        movies, positions = self.fetch_indexes()
+        relations = self.get_relations(positions, max_relations)
+        return self.store_relations_for([ movie_id ], movies, relations)
+
     def fetch_indexes(self):
         query = SongIndex.select().where(
             SongIndex.version == settings['model_version']
@@ -25,8 +31,6 @@ class SongRelationConstructor:
         kdt = KDTree(positions)
         origins = np.array([[i for j in range(k)] for i in range(positions.shape[0])])
         distances, destinations = kdt.query(positions, k = k)
-        # distances.shape = positions.shape[:-1] + (k,)
-        # destinations.shape = positions.shape[:-1] + (k,)
         relations = np.array((origins, destinations, distances*1000), dtype=np.int32).reshape((3,-1)).T
         mask = [ False
                 if (k[1::-1] == relations[...,0:2]).all(axis=-1).any()
@@ -36,10 +40,15 @@ class SongRelationConstructor:
         return relations[mask]
 
     def store_all_relations(self, movies, relations):
+        return self.store_relation(movies, movies, relations)
+
+    def store_relations_for(self, target_movie_ids, movies, relations):
         '''
         In:
+            target_movie_ids: iterable(dtype=str)
+                iterable object contains movie_ids to store relations
             movies: np.array(dtype=str)
-                nparray for movie_ids
+                nparray for movie_ids included in relations
             relations: np.array(dtype=np.int32, shape=(3, -1))
                 nparray contains origin, destinations, distance
                 each origin/destinations value are indices of movie
@@ -64,10 +73,15 @@ class SongRelationConstructor:
         pb = ProgressBar()
 
         with database.atomic():
-            for movie in pb( movies ):
-                origin_id = movie
-                movie_id_index = pb.value
-                related_relations = relations[relations[...,0] == movie_id_index]
+            for origin_id in pb(target_movie_ids):
+                origin_id_index = np.argwhere(movies == origin_id).reshape(-1)
+                if origin_id_index.shape[0] == 0:
+                    root_logger.warn('Skipped for not found movie {}. Maybe not analyzed?'.format(origin_id))
+                    continue
+
+                related_relations = relations[relations[...,0] == origin_id_index[0]]
+                if related_relations.shape[0] == 0:
+                    continue
 
                 existing_relations = Origin.select(
                     Destination.status
@@ -93,3 +107,4 @@ class SongRelationConstructor:
             if len(ssr_target) != 0:
                 StatusSongRelation.insert_many(ssr_target).execute()
 
+        return len(ssr_target) // 2
