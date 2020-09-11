ad4d27c40232dab2a5a014be7ec2436937228cbe
Nanamachi <7machi@nanamachi.net>
2020-09-11 23:50:55 +0900

    fix(SRel.Const.): recalculate didn't work if movie is dest.

--- utako/presenter/song_relation_constructor.py
+++ utako/presenter/song_relation_constructor.py
@@ -9,28 +9,28 @@ from utako.model.status_song_relation import StatusSongRelation
 from sklearn.neighbors import KDTree
 
 class SongRelationConstructor:
-    def __call__(self, max_relations = 11):
-        movies, positions = self.fetch_indexes()
-        relations = self.get_relations(positions, max_relations)
-        self.store_all_relations(movies, relations)
+    def __call__(self, max_relations = 13):
+        self.construct_kdtree()
+        relations = self.get_all_relations(max_relations)
+        return self.store_relations(relations)
 
-    def recalculate_for(self, movie_id, max_relations=11):
-        movies, positions = self.fetch_indexes()
-        relations = self.get_relations(positions, max_relations)
-        return self.store_relations_for([ movie_id ], movies, relations)
+    def recalculate_for(self, movie_id, max_relations=13):
+        self.construct_kdtree()
+        relations = self.get_relations_from_movie_id(movie_id, max_relations)
+        return self.store_relations(relations)
 
-    def fetch_indexes(self):
+    def construct_kdtree(self):
         query = SongIndex.select().where(
             SongIndex.version == settings['model_version']
         ).tuples()
-        movies = np.array([q[1] for q in query], dtype=str)
-        positions = np.array([q[2:10] for q in query], dtype=np.float64)
-        return movies, positions
 
-    def get_relations(self, positions, k):
-        kdt = KDTree(positions)
-        origins = np.array([[i for j in range(k)] for i in range(positions.shape[0])])
-        distances, destinations = kdt.query(positions, k = k)
+        self.movies = np.array([q[1] for q in query], dtype=str)
+        self.positions = np.array([q[2:10] for q in query], dtype=np.float64)
+        self.kd_tree = KDTree(self.positions)
+
+    def get_all_relations(self, k):
+        origins = np.array([[i for j in range(k)] for i in range(self.positions.shape[0])])
+        distances, destinations = self.kd_tree.query(self.positions, k = k)
         relations = np.array((origins, destinations, distances*1000), dtype=np.int32).reshape((3,-1)).T
         mask = [ False
                 if (k[1::-1] == relations[...,0:2]).all(axis=-1).any()
@@ -39,32 +39,50 @@ class SongRelationConstructor:
                 for k in relations]
         return relations[mask]
 
-    def store_all_relations(self, movies, relations):
-        return self.store_relation(movies, movies, relations)
+    def get_relations_from_movie_id(self, movie_id, k):
+        origin_index = self.get_index_from_movie_id(movie_id)
+        distances, destinations = self.kd_tree.query(self.positions[origin_index].reshape(1,-1), k = k)
 
-    def store_relations_for(self, target_movie_ids, movies, relations):
+        origin_index_map = np.array([ origin_index for _ in range(k) ]).reshape(1,-1)
+
+        relations = np.array((
+            origin_index_map,
+            destinations,
+            distances*1000
+        ), dtype=np.int32).reshape((3,-1)).T
+
+        mask = ( destinations != origin_index ).reshape(-1)
+
+        return relations[mask]
+
+    def get_index_from_movie_id(self, movie_id):
+        movie_index = np.argwhere(self.movies == movie_id).reshape(-1)
+
+        if movie_index.shape[0] != 1:
+            raise ValueError
+        else:
+            return movie_index[0]
+
+    def store_relations(self, relations):
         '''
         In:
-            target_movie_ids: iterable(dtype=str)
-                iterable object contains movie_ids to store relations
-            movies: np.array(dtype=str)
-                nparray for movie_ids included in relations
             relations: np.array(dtype=np.int32, shape=(3, -1))
                 nparray contains origin, destinations, distance
                 each origin/destinations value are indices of movie
-        Out: (None)
+        Out: inserted_count: int
+            number of newly created song_relation record
         '''
         ssr_target = []
-        def create(movies, rel):
+        def create(rel):
             record = SongRelation.create(
                 distance = rel[2],
                 version = settings['model_version']
             )
             return [{
-                'status': movies[rel[0]],
+                'status': self.movies[rel[0]],
                 'song_relation': record.id
             },{
-                'status': movies[rel[1]],
+                'status': self.movies[rel[1]],
                 'song_relation': record.id
             }]
 
@@ -73,15 +91,9 @@ class SongRelationConstructor:
         pb = ProgressBar()
 
         with database.atomic():
-            for origin_id in pb(target_movie_ids):
-                origin_id_index = np.argwhere(movies == origin_id).reshape(-1)
-                if origin_id_index.shape[0] == 0:
-                    root_logger.warn('Skipped for not found movie {}. Maybe not analyzed?'.format(origin_id))
-                    continue
-
-                related_relations = relations[relations[...,0] == origin_id_index[0]]
-                if related_relations.shape[0] == 0:
-                    continue
+            for origin_index in pb(np.unique(relations[...,0])):
+                origin_id = self.movies[origin_index]
+                related_relations = relations[relations[...,0] == origin_index]
 
                 existing_relations = Origin.select(
                     Destination.status
@@ -95,14 +107,14 @@ class SongRelationConstructor:
                     Origin.status == origin_id,
                     SongRelation.version == settings['model_version'],
                 ).tuples()
-                existing_movie_id_indices = np.argwhere(np.isin(movies, tuple(existing_relations)))
+                existing_movie_id_indices = np.argwhere(np.isin(self.movies, tuple(existing_relations)))
 
                 new_relation = related_relations[
                     np.logical_not(np.isin(related_relations[...,1], existing_movie_id_indices))
                 ]
 
                 flatten = lambda l: sum(l, [])
-                ssr_target += flatten([ create(movies, rel) for rel in new_relation ])
+                ssr_target += flatten([ create(rel) for rel in new_relation ])
 
             if len(ssr_target) != 0:
                 StatusSongRelation.insert_many(ssr_target).execute()
